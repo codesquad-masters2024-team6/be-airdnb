b2784a508ac5771dcda4cc0ea9a1a70ee1b8feb6
imojaek <imojaek@gmail.com>
2024-06-04 18:12:58 +0900

    [refactor/#7] refactor: 어매니티 관련 테이블 구조 변경

--- backend/src/main/java/codesquad/airdnb/domain/accommodation/AccoAmen.java
+++ backend/src/main/java/codesquad/airdnb/domain/accommodation/AccoAmen.java
@@ -1,18 +1,10 @@
 package codesquad.airdnb.domain.accommodation;
 
+
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Table(name = "ACCO_PHOTO")
-public class AccoPhoto {
+public class AccoAmen {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +14,7 @@ public class AccoPhoto {
     @JoinColumn(name = "ACCO_ID")
     private Accommodation accommodation;
 
-    private String url;
+    @ManyToOne
+    @JoinColumn(name = "AMEN_ID")
+    private Amenity amenity;
 }
