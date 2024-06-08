d031200dc1bbe28998fffc92e07c9c7181b3a110
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-06-08 21:57:01 +0900

    [feat/#10] feat: footer 디자인 수정

--- frontend/src/components/HostingFooter.svelte
+++ frontend/src/components/HostingFooter.svelte
@@ -1,29 +1,28 @@
-<footer class="bg-white rounded-lg shadow m-4">
+<footer class="bg-white rounded-lg mt-20">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+            <a href="/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap">Airdnb</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
                 <li>
                     <a href="/" class="hover:underline me-4 md:me-6">About</a>
                 </li>
                 <li>
-                    <a href="/" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    <a href="https://github.com/imojaek" class="hover:underline me-4 md:me-6">Dao</a>
                 </li>
                 <li>
-                    <a href="/" class="hover:underline me-4 md:me-6">Licensing</a>
+                    <a href="https://github.com/seedspirit" class="hover:underline me-4 md:me-6">Sid</a>
                 </li>
                 <li>
-                    <a href="/" class="hover:underline">Contact</a>
+                    <i class="fa-brands fa-github"></i>
+                    <a href="https://github.com/codesquad-masters2024-team6/be-airdnb/tree/dev" class="hover:underline">GitHub</a>
                 </li>
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center">
-            © 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>
-            . All Rights Reserved.
+            © 2024 Dao & Sid. All Rights Reserved.
         </span>
     </div>
 </footer>
