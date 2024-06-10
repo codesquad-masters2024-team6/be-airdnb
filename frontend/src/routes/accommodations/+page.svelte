8207ca3a26dbbe806cc22995695e3b7ca8c39f33
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-06-10 13:35:14 +0900

    [feat/#10] style: 숙소 조회 레이아웃 설정

---
+++ frontend/src/routes/accommodations/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col min-h-screen">
+    <header>
+        <div id="logo" class="w-[1440px] flex items-center justify-between p-4 py-6">
+            <a class="text-2xl font-semibold" href="/hosting">Airdnb</a>
+        </div>
+    </header>
+    <section class=" grow flex">
+        <section class="grow-[1]">
+            숙소 리스트
+        </section>
+        <section class="grow-[1]">
+            지도
+        </section>
+    </section>
+</div>
