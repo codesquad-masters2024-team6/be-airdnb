657988a5ca799bbc97718f7844c8a829138d39d7
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-06-10 12:56:31 +0900

    [feat/#10] style: 숙소 등록 레이아웃 정리

--- frontend/src/components/LocationForm.svelte
+++ frontend/src/components/LocationForm.svelte
@@ -3,10 +3,11 @@
     let countries = ["한국 - KR", "영국 - UK", "터키 - TR"];
 </script>
 
-<div class="mx-auto p-8 mt-10">
+<div class="mx-auto p-2 mt-5">
     <div class="mb-4">
         <label for="country" class="block text-sm font-medium text-gray-700 mb-1">국가/지역</label>
-        <select id="country" name="country" class="mt-1 block w-full py-2 px-3 border border-gray-400 bg-white rounded-md shadow-sm
+        <select id="country" name="country" class="mt-1 block w-full py-2 px-3
+        border border-gray-400 bg-white rounded-md shadow-sm
         focus:border-2 focus:ring-blue-700 focus:border-blue-700 sm:text-sm">
             {#each countries as country}
             <option>{country}</option>
