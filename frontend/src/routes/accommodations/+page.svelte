a1834e79ba44b2d5269422dc349801f0d0530690
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-06-10 16:45:40 +0900

    [feat/#10] feat: 숙소 상세 조회를 위한 카드UI 추가

--- frontend/src/routes/accommodations/+page.svelte
+++ frontend/src/routes/accommodations/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import AccoCard from "./AccoCard.svelte";
+</script>
+
 <div class="flex flex-col min-h-screen">
     <header>
         <div id="logo" class="w-[1440px] flex items-center justify-between p-4 py-6">
@@ -6,7 +10,17 @@
     </header>
     <section class=" grow flex">
         <section class="grow-[1]">
-            숙소 리스트
+            <div class="flex flex-wrap gap-5 content-start">
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+                <AccoCard />
+            </div>
         </section>
         <section class="grow-[1]">
             지도
