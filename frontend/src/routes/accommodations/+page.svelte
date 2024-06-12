bbb9af55aaf412c88114ac476b260c261fd53215
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-06-12 13:02:48 +0900

    [feat/#10] feat: 숙소 검색 화면 레이아웃 설정

--- frontend/src/routes/accommodations/+page.svelte
+++ frontend/src/routes/accommodations/+page.svelte
@@ -1,29 +1,57 @@
 <script>
     import AccoCard from "./AccoCard.svelte";
+        import {onMount} from "svelte";
+
+        onMount(() => {
+        const container = document.getElementById('map');
+        const options = {
+        center: new kakao.maps.LatLng(37.490823, 127.033435),
+        level: 3
+    };
+        const map = new kakao.maps.Map(container, options);
+
+        // 지도를 클릭한 위치에 표출할 마커입니다
+        let marker = new kakao.maps.Marker({
+        // 지도 중심좌표에 마커를 생성합니다
+        position: map.getCenter()
+    });
+        // 지도에 마커를 표시합니다
+        marker.setMap(map);
+
+        // 지도에 클릭 이벤트를 등록합니다
+        // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+        kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+        // 클릭한 위도, 경도 정보를 가져옵니다
+        let latlng = mouseEvent.latLng;
+
+        // 마커 위치를 클릭한 위치로 옮깁니다
+        marker.setPosition(latlng);
+        let message = '위도는 ' + latlng.getLat() + ' 이고, ';
+        message += '경도는 ' + latlng.getLng() + ' 입니다';
+        let resultDiv = document.getElementById('clickLatlng');
+        resultDiv.innerHTML = message;})
+    })
 </script>
 
 <div class="flex flex-col min-h-screen">
     <header>
-        <div id="logo" class="w-[1440px] flex items-center justify-between p-4 py-6">
+        <div id="logo" class="w-full flex items-center justify-between p-4 py-6">
             <a class="text-2xl font-semibold" href="/hosting">Airdnb</a>
         </div>
     </header>
-    <section class=" grow flex">
-        <section class="grow-[1]">
-            <div class="flex flex-wrap gap-5 content-start">
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-                <AccoCard />
-            </div>
+    <section class="flex h-screen">
+        <section class="flex gap-1 w-[720px] flex-wrap overflow-scroll p-2">
+            <AccoCard/>
+            <AccoCard/>
+            <AccoCard/>
+            <AccoCard/>
+            <AccoCard/>
+            <AccoCard/>
+            <AccoCard/>
         </section>
-        <section class="grow-[1]">
-            지도
+        <section class="bg-pink-500 w-[720px]">
+            <div id="map" class="w-full h-full"></div>
+            <div id="clickLatlng" class="mt-4"></div>
         </section>
     </section>
 </div>
--- frontend/src/routes/accommodations/AccoCard.svelte
+++ frontend/src/routes/accommodations/AccoCard.svelte
@@ -1,17 +1,21 @@
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-    <a href="https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920">
-        <img class="rounded-t-lg" src="https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920" alt="" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-            Read more
-            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
+<!--<div class="max-w-[720px] bg-white border border-gray-200 rounded-lg shadow">-->
+<!--    <img class="rounded-t-lg object-cover"-->
+<!--         src="https://a0.muscache.com/im/pictures/5e67688b-757d-44d6-8b4b-1e91dc6fe49f.jpg?im_w=1920"-->
+<!--         alt="" />-->
+<!--    <div class="">-->
+<!--        <a href="#">-->
+<!--            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Noteworthy technology acquisitions 2021</h5>-->
+<!--        </a>-->
+<!--        <p class="mb-3 font-normal text-gray-700">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>-->
+<!--    </div>-->
+<!--</div>-->
+
+<div class="w-full flex bg-pink-500 border border-gray-200 rounded-lg shadow">
+    <img src="https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/bltfbcc7f32e0cd6ff5/getting-started-on-airbnb-optimized.jpg"
+         class="object-cover w-[400px] h-full p-3 rounded-lg"
+         alt="이미지~">
+    <div class="flex flex-col">
+        <p class="mb-2 text-2xl font-bold tracking-tight text-gray-900">숙소 제목</p>
+        <p class="mb-3 font-normal text-gray-700">숙소 설명</p>
     </div>
 </div>
